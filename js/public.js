f3bc90fe5e6ef4a7f23efac465fc641b4b2f6b19
SaSaChu <teresa7397@gmail.com>
2016-08-16 00:58:02 +0800

    oa展示下拉JQ。

--- js/public.js
+++ js/public.js
@@ -1,4 +1,17 @@
 
 $(function () {
+  $('.box .c, .box .f').click (function () {
+    if ($(this).parent ().hasClass ('A')) {
+      $(this).parent ().removeClass ('A');
+      $(this).parent ().addClass ('B');
+    } else {
+      $(this).parent ().removeClass ('B');
+      $(this).parent ().addClass ('A');
+    }
+  });
+  append
+
+
+
 
 });
