78eb5a3fcfbd1f6a2c2b8711d1a904a610343c97
SaSaChu <teresa7397@gmail.com>
2017-05-13 15:44:12 +0800

    Finsh

--- js/public.js
+++ js/public.js
@@ -1,4 +1,10 @@
+// 打出外面的東西：$() + function + () + {} + ;
 
 $(function () {
 
+  $('#btn').click(function() {
+    // 驗證動作有沒有效
+    // console.log('123');
+    $('.box').removeClass('a b c d');
+  });
 });
