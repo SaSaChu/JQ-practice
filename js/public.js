6b3201b5c3eb64f6ec65fc35149628b1953517d3
SaSaChu <teresa7397@gmail.com>
2017-05-15 23:40:52 +0800

    解答

--- js/public.js
+++ js/public.js
@@ -1,11 +1,14 @@
 
 $(function(){
+  $('.rboxs').empty(); 
+  // 意思等同於 $('.rboxs').text(''); 空值。這用法是需要設定在最外層，用意是為了要在一開始把html裡面清空
 
   $('button').click(function(){
 
-    var $v = $('#in').val();
+    var v = $('#in').val(); // 元素？字串？
+
     var $div = $('<div />').addClass('rbox');
-    var $p = $('<p />').addClass('se').text($v);
+    var $p = $('<p />').addClass('se').text(v);
     var $a = $('<a />').text('Detel');
 
 
@@ -16,8 +19,13 @@ $(function(){
 
     $p.click(function() {
 
-      prompt("請輸入你想說的");
+      var a = prompt("請輸入你想說的");
+      // $(this).text(a); 這也是對的用法
+      // $p.appndTo (a); 這也是對的用法
+      $p.text(a);
 
+      // a.appendTo ($p); 這是錯誤的用法
+      // a 是字串，字串是沒有appendTo的function的
     });
 
     $a.click(function() {
