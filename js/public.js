af49b3a2a6322562ea5b850a5ecd987d2002414c
SaSaChu <teresa7397@gmail.com>
2017-05-16 22:08:44 +0800

    寫完作業

--- js/public.js
+++ js/public.js
@@ -1,4 +1,53 @@
 
-$(function () {
+
+$(function() {
+
+  $('.downboxs').empty();
+  // console.log('12345');
+
+  $('button').click(function() {
+
+    var type = $('#in').val();
+    var $div = $('<div />').addClass('downbox');
+    var $span = $('<span />').text(type);
+    var $linkboxs = $('<div />').addClass('typebox');
+    var $a = $('<div />').append($('<a />').addClass('a').text('變色'));
+    var $b = $('<div />').append($('<a />').addClass('b').text('修改'));
+    var $c = $('<div />').append($('<a />').addClass('c').text('刪除'));
+
+    $('.downboxs').append($div
+      .append($span)
+      .append($linkboxs
+        .append($a).append($b).append($c)
+        )
+      );
+
+    // 變色
+    $a.click(function(){
+      $div.addClass('ch');
+    });
+
+
+    // 修改
+    $b.click(function(){
+      var e = prompt('請輸入你要修改的');
+      $span.text(e);
+    });
+
+
+    // 刪除 
+    $c.click(function() {
+      $div.remove();
+    });
+
+
+    $('#in').val('');
+
+  });
 
 });
+
+
+
+
+
