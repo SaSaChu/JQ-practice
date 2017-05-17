35e69cc682606e726a9145328bb0a02b7bb969a1
SaSaChu <teresa7397@gmail.com>
2017-05-18 01:32:12 +0800

    一小時半完成功能，缺少為輸入文字之表單設定。

--- js/public.js
+++ js/public.js
@@ -1,4 +1,54 @@
 
 $(function () {
+  
+  $('.down').empty('');
+
+  $('button').click(function () {
+    // console.log('11111');
+    var type = $('#in').val();
+    var $div = $('<div />').addClass('d_boxs');
+    var $span = $('<span />').text(type);
+    var $aboxs = $('<div />').addClass('a_boxs');
+    var $a = $('<div />').addClass('a').text('變色');
+    var $b = $('<div />').addClass('b').text('修改');
+    var $c = $('<div />').addClass('c').text('刪除');
+
+
+    $('.down').append($div
+      .append($span).append($aboxs
+        .append($a).append($b).append($c))
+      );
+
+    // 變色
+    $a.click(function() {
+      if ($div.hasClass('cc')) {
+        $div.removeClass('cc');
+      } else {
+        $div.addClass('cc');
+      }
+    });
+
+    // 修改
+    $b.click(function() {
+      var e = prompt('請輸入要修改的字串');
+      $span.text(e);
+    });
+
+    // 刪除 
+    $c.click(function(){
+      $div.remove();
+    });
+
+    $('#in').val('');
+
+  });
 
 });
+
+
+
+
+
+
+
+
