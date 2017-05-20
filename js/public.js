60efa0587f35efdd7ea47abfea83a6b98ab10823
SaSaChu <teresa7397@gmail.com>
2017-05-20 23:35:24 +0800

    解答

--- js/public.js
+++ js/public.js
@@ -1,28 +1,36 @@
 
 $(function() {
+  var ans = 55;
+  var max = 100;
+  var min = 0;
 
   $('button').click(function() {
     // console.log('1234');
     var type = $('#in').val();
 
-    if(type === '' && type === null) {
+    if(type === '') {
       $('.down').text('請輸入數字呦！');
+    } else {
+
+      if (isNaN (type)) { // isNaN 是否 不是數字 Not a Number
+        $('.down').text('請輸入數字呦！');
+      } else {
+        if (type == ans) {
+          $('.down').text('恭喜你答對了！');
+        }
+
+        if (type > ans) {
+          $('.down').text('終極密碼在' + min + ' ~ ' + type + '的數字喔！');
+          max = type; // 記錄你輸入的最大值
+        }
+
+        if (type < ans) {
+          $('.down').text('終極密碼在' + type + ' ~ ' + max + '的數字喔！');
+          min = type; // 記錄你輸入的最小值
+        }
+      }
     }
 
-    if (type == 55) {
-      $('.down').text('恭喜你答對了！');
-    }
-
-    if (type > 55) {
-      $('.down').text('終極密碼在0 ~ ' + type + '的數字喔！');
-    }
-
-    if (type < 55) {
-      $('.down').text('終極密碼在' + type + ' ~ 100的數字喔！');
-    }
-    // else {
-    //   $('.down').text('終極密碼在'+'55-100'+'的數字喔！');
-    // }
 
     $('#in').val('');
 
