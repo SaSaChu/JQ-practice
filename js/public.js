7b862dbb692ab2d54ee431c3146977bd63eb8673
SaSaChu <teresa7397@gmail.com>
2017-06-17 16:25:08 +0800

    bmi練習

--- js/public.js
+++ js/public.js
@@ -1,4 +1,18 @@
 
 $(function () {
+    
+  function ans (a, b) {
+    b = b / 100;
+    var t = a / (b * b);
+    return t;
+  }
+
+  $('button').click(function () {
+    var w = $('#weight').val();
+    var h = $('#height').val();
+    var bmi = ans(w, h);
+    $('.d_boxs').text (bmi);
+  });
+
 
 });
