c3237baceadd805e2d5875c1bfd931d072dfbdef
SaSaChu <teresa7397@gmail.com>
2017-06-17 15:15:43 +0800

    function的解說

--- js/public.js
+++ js/public.js
@@ -1,4 +1,16 @@
 
 $(function () {
 
+  function AA (a, b) {
+    b = b / 100; //m，換算成公尺，因為JS只接受公尺
+    var t = a / (b * b);
+    return t; //return = 回傳的意思
+  }
+
+  var w = 70;
+  var h = 170; //cm
+  var bmi = AA(w, h);
+
+  console.log(bmi);
+  
 });
