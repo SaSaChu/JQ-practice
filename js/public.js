5a3372b062014d8ca9dd873e2930bd722bc63c28
SaSaChu <teresa7397@gmail.com>
2017-06-17 17:09:00 +0800

    over+檢討完了

--- js/public.js
+++ js/public.js
@@ -1,4 +1,33 @@
 
 $(function () {
 
+  function create_box(v) {
+
+    var $div = $('<div />');
+
+    var $span = $('<span />');
+    $span.text(v);
+
+    var $a = $('<a />');
+    $a.text('刪除');
+    $a.click(function() {
+      $(this).parent().remove();
+    });
+
+    var $t = $div.append($span).append($a);
+
+    return $t;
+  }
+
+
+  $('button').click(function() {
+
+    var type = $('#in').val();
+    var ans = create_box(type);
+
+    $('.d_boxs').append(ans);
+
+    $('#in').val('');
+  });
+  
 });
